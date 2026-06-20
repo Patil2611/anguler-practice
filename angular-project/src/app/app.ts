3fac84c855201aca25b1e498329ddbb6f1b3638f
bp4433905@gmail.com <bp4433905@gmail.com>
2026-06-20 23:46:29 +0530

    Test meterial aded for button

--- angular-project/src/app/app.ts
+++ angular-project/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./navbar/navbar";
 import { Footer } from "./footer/footer";
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, MatButtonModule, MatBadgeModule, MatBadgeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
