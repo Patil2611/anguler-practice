6c064531f148ce2ed8a18e90ecd9e693526fd3bf
bp4433905@gmail.com <bp4433905@gmail.com>
2026-06-21 16:26:11 +0530

    added about file and restructure app

--- angular-project/src/app/navbar/navbar.ts
+++ angular-project/src/app/navbar/navbar.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
